87c43d76e4a3055d670af0bd170586c6854e4728
Mauricio Rodrigues Cruz <brcruz@gmail.com>
2012-02-24 16:09:05 -0200

    memory reporter and process analyzer implemented. Also, cleaned some confuse code.

--- cli/performancereport.py
+++ cli/performancereport.py
@@ -2,7 +2,6 @@ import sys
 import os
 if os.name != 'posix':
     sys.exit('platform not supported')
-import curses
 import atexit
 import time
 import commands
@@ -11,18 +10,6 @@ import string
 from threading import Thread
 
 import psutil
-
-# --- curses stuff
-def tear_down():
-    win.keypad(0)
-    curses.nocbreak()
-    curses.echo()
-    curses.endwin()
-
-win = curses.initscr()
-atexit.register(tear_down)
-curses.endwin()
-lineno = 0
 
 def toKbps(n):
     return float(n >> 7)
@@ -33,7 +20,7 @@ def trunc(f, n):
     return str(f)[:slen]
 
 def dataFormater(dataList, formats):
-	'''receives a data list and a format list returning the medium value until de Nth position of each format item'''
+	'''receives a data list and a format list returning the medium value until de Nth position of the data list for each format item'''
 	returnList = []
 	for form in formats:
 		i = 0
@@ -68,8 +55,8 @@ def sendReport(destination, service, state, message):
 		+ send_nsca_cfg_dir + "/send_nsca.cfg" 
 		)
 
-	commandoutput = commands.getoutput(command)
-	win.refresh()
+	#commandoutput = commands.getoutput(command)
+	#print message
 
 class CircularList:
 	#TOFIX: the circular list is not a temporal circular, it is a memory allocation circular.
@@ -89,10 +76,49 @@ class CircularList:
 		
 	def GetList(self):
 		return self.dataList
+		
+class processesAnalyzer(Thread):
+	'''collect cpu process data and print them to the stdout'''
+	def __init__ (self,refreshRate):
+		Thread.__init__(self)
+		self.refreshRate = refreshRate
+		self.terminate = False
+	
+	def kill(self):
+		self.terminate = True
+		
+	def run(self):
+		while True:
+			
+			if self.terminate:
+				return 
+			
+			processList = []
+			for p in psutil.process_iter():
+				processList.append(p)
+			
+			processesSortByMem = sorted(processList, key=lambda p: p.get_memory_percent(), reverse=True)
+			processesSortByProc = sorted(processList, key=lambda p: p.get_cpu_percent(interval=0), reverse=True)
+			
+			printProcStatus = False
+			if printProcStatus:
+				print "sorted by memory usage"
+				for i, p in zip(range(0,5),processesSortByMem):
+					print (" process name: " + str(p.name) + 
+							" mem use: " + str(p.get_memory_percent()))
+				print "\n"
+				print "sorted by processor usage"
+				for i, p in zip(range(0,5),processesSortByProc):
+					print (" process name: " + str(p.name) + 
+							" proc use: " + str(p.get_cpu_percent(interval=0)))
+				print "\n\n\n\n\n\n\n\n"
+			time.sleep(self.refreshRate)
 
-class processorReporter(Thread):
+class memoryReporter(Thread):
+	'''thread class to collect and report memory data'''
 	def __init__ (self,refreshRate, sendRate, destination, formatList, timeLapseSize):
 		Thread.__init__(self)
+		self.terminate = False
 		self.refreshRate = refreshRate
 		self.sendRate = sendRate
 		self.destination = destination
@@ -101,37 +127,91 @@ class processorReporter(Thread):
 		self.warn = "50.000"
 		self.crit = "90.000"
 		self.mini = "0"
+		self.service = "Memory Performance Report"
+		self.state= "3"
+		self.host = "`ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}'`"
+		
+	def kill(self):
+		self.terminate = True
+		
+	def run(self):
+		memDataList = CircularList(self.timeLapseSize)
+		
+		while True:
+			
+			if self.terminate:
+				return
+			
+			sendTime = self.sendRate	
+			while sendTime >= 0:
+				sendTime -= 1
+				time.sleep(self.refreshRate)
+				memUse = psutil.phymem_usage().percent
+				memDataList.Append(memUse)
+			
+			formatedData = dataFormater(memDataList.GetList(), self.formatList)
+			message = "mem usage:" + str(formatedData)
+			message += "|"
+			
+			for Format, Data in zip(self.formatList, formatedData) :
+				message += "muse" + str(Format) + "=" + str(Data) + "%;" + self.warn + ";" + self.crit + ";"  + self.mini + "; "
+			
+			sendReport(self.destination, self.service, self.state, message)
+			
+class processorReporter(Thread):
+	'''thread class to collect and report processor data'''
+	def __init__ (self,refreshRate, sendRate, destination, formatList, timeLapseSize):
+		Thread.__init__(self)
+		self.terminate = False
+		self.refreshRate = refreshRate
+		self.sendRate = sendRate
+		self.destination = destination
+		self.formatList = formatList
+		self.timeLapseSize = timeLapseSize
+		self.warn = "50.000"
+		self.crit = "90.000"
+		self.mini = "0"
+		self.service = "Processor Performance Report"
+		self.state= "3"
+		self.host = "`ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}'`"
+
+	def kill(self):
+		self.terminate = True
       
 	def run(self):
-		'''thread function to collect and report processor data'''
 		cpuDataList = CircularList(self.timeLapseSize)
 		while True:
+			
+			if self.terminate:
+				return
+			
 			sendTime = self.sendRate
 			while sendTime >= 0:
-				sendTime = sendTime - 1
+				sendTime -= 1
 				cpu_quantity = 0
 				cpu_percent = 0
 				cpu_quantity = cpu_quantity + 1
-				cpuDataList.Append(psutil.cpu_percent(interval=0.1, percpu=False))
-
+				cpuDataList.Append(psutil.cpu_percent(self.refreshRate, percpu=False))
+			
 			formatedData = dataFormater(cpuDataList.GetList(), self.formatList)
 			
-			service = "Processor Performance Report"
-			state= "3"
-			host = "`ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}'`"
 			#example:
 			#printf "%s\t%s\t%s\t%s\n" "192.168.0.100" "CPU Load" "0" "Status information|cpu1=50%;90;95;0;100" | ~/downloads/nsca-2.7.2/src/send_nsca -H 192.168.0.100 -c ~/downloads/nsca-2.7.2/sample-config/send_nsca.cfg
+			
 			message = "cpu load:" + str(formatedData)
 			message += "|"
+			
 			for Format, Data in zip(self.formatList, formatedData) :
 				message += "load" + str(Format) + "=" + str(Data) + "%;" + self.warn + ";" + self.crit + ";"  + self.mini + "; "
 				#example:
 				#load1=0.040;5.000;10.000;0; load5=0.010;4.000;6.000;0; load15=0.000;3.000;4.000;0;
-			sendReport(self.destination, service, state, message)
+			sendReport(self.destination, self.service, self.state, message)
 
 class networkReporter(Thread):
+	'''thread class to collect and report network data'''
 	def __init__ (self, refreshRate, sendRate, destination, formatList, timeLapseSize):
 		Thread.__init__(self)
+		self.terminate = False
 		self.refreshRate = refreshRate
 		self.sendRate = sendRate
 		self.destination = destination
@@ -140,17 +220,24 @@ class networkReporter(Thread):
 		self.warn = "400.000"
 		self.crit = "900.000"
 		self.mini = "0"
-      
+		self.service = "Network Performance Report"
+		self.state= "3"
+		self.host = "`ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}'`"
+		      
+	def kill(self):
+		self.terminate = True
+	
 	def run(self):
-		'''thread function to collect and report network data'''
 		sentDataList = CircularList(self.timeLapseSize)
 		receivedDataList = CircularList(self.timeLapseSize)
 		while True:
-			sendTime = self.sendRate
 			
+			if self.terminate:
+				return
+			
+			sendTime = self.sendRate
 			while sendTime >= 0:
-				sendTime = sendTime - 1
-				
+				sendTime -= 1
 				#collect data
 				tot_before = psutil.network_io_counters()
 				pnic_before = psutil.network_io_counters(pernic=True)
@@ -162,7 +249,6 @@ class networkReporter(Thread):
 				name = pnic_after.keys()[1] #get one network interface only
 				stats_before = pnic_before[name]
 				stats_after = pnic_after[name]
-				
 				bytesSent = toKbps(stats_after.bytes_sent - stats_before.bytes_sent)
 				bytesReceived = toKbps(stats_after.bytes_recv - stats_before.bytes_recv)
 				
@@ -173,11 +259,7 @@ class networkReporter(Thread):
 			formatedSentData = dataFormater(sentDataList.GetList(), self.formatList)
 			formatedReceivedData = dataFormater(receivedDataList.GetList(), self.formatList)
 			
-			service = "Network Performance Report"
-			state= "3"
-			host = "`ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}'`"
-			message = host + " Received data " + str(formatedReceivedData) + " Sent data " + str(formatedSentData)
-			
+			message = " Received data " + str(formatedReceivedData) + " Sent data " + str(formatedSentData)
 			message = "sent traffic:" + str(formatedSentData) + " received traffic:" + str(formatedReceivedData)
 			message += "|"
 			for Format, SentData, ReceivedData in zip(self.formatList, formatedSentData, formatedReceivedData) :
@@ -185,19 +267,19 @@ class networkReporter(Thread):
 				message += "sent" + str(Format) + "=" + str(SentData) + "kbps;" + self.warn + ";" + self.crit + ";"  + self.mini + "; "
 				#example:
 				#load1=0.040;5.000;10.000;0; load5=0.010;4.000;6.000;0; load15=0.000;3.000;4.000;0;
-			sendReport(self.destination, service, state, message)
+			
+			sendReport(self.destination, self.service, self.state, message)
 	
-def refresh_window():
+def main_loop():
 	'''main loop to call all the reporters'''
 	
-	print "Main Loop called"
+	#temporary parameters definition
 	destination = "143.54.12.174"
 	threadsList = []
-	refreshRate = 0.1
-	sendRate = 20
+	refreshRate = 0.1 #seconds
+	sendRate = 20 #ticks -> relative to the refreshRate parameter
 	formatList = [3,15,60]
 	timeLapseSize = 60
-	
 	
 	#here we should have the main call to the reporter threads
 	
@@ -209,13 +291,27 @@ def refresh_window():
 	current = processorReporter(refreshRate, sendRate, destination, formatList, timeLapseSize)
 	threadsList.append(current)
 	
+	#memoryReporter thread
+	current = memoryReporter(refreshRate, sendRate, destination, formatList, timeLapseSize)
+	threadsList.append(current)
+	
+	#processesAnalyzer thread
+	current = processesAnalyzer(1)
+	threadsList.append(current)
+	
+	#start every thread
 	for reporterThread in threadsList:
 		reporterThread.start()
-
-	input("sair")
 	
+	raw_input("Press Enter to kill all threads...")
+
+	#send kill sign to all threads
 	for reporterThread in threadsList:
-		reporterThread._Thread__stop()
+		reporterThread.kill()
+	
+	#wait for each thread to finish
+	for reporterThread in threadsList:
+		reporterThread.join()
 	
 def main():
     try:
@@ -228,7 +324,7 @@ def main():
 			while 1:
 				refresh_window()
 		'''
-		refresh_window()
+		main_loop()
     except (KeyboardInterrupt, SystemExit):
         pass
 
