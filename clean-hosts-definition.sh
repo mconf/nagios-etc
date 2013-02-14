1f283ac63664fba4662c088a26e8f12b3aef88dc
Felipe Cecagno <fcecagno@gmail.com>
2013-02-14 14:51:56 -0200

    updating some configuration files

--- clean-hosts-definition.sh
+++ clean-hosts-definition.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-cat /usr/local/nagios/etc/objects/mconf/hosts.cfg | grep -v '#.*\|^$' | sed 's:}:}\n:g' > hosts.cfg.tmp
-sudo mv hosts.cfg.tmp /usr/local/nagios/etc/objects/mconf/hosts.cfg
+cp nagios/etc/objects/mconf/hosts.cfg nagios/etc/objects/mconf/hosts.cfg.backup
+cat nagios/etc/objects/mconf/hosts.cfg | grep -v '#.*\|^$' | sed 's:}:}\n:g' > hosts.cfg.tmp
+sudo mv hosts.cfg.tmp nagios/etc/objects/mconf/hosts.cfg
